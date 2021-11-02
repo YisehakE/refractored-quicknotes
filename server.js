90aad9a6761f9165d39baf3bd8f901732d938573
Yisehak Ebrahim <yisehakebrahim02@gmail.com>
2021-11-02 14:16:34 -0400

    Fixing path in server and adding build files

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express")
 
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 7000;
 
 app.use(express.static(path.join(__dirname, "build")));
 
-app.length("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
+app.get("/*", (req, res) => {
+    res.sendFilne(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(PORT);
